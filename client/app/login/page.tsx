e2d8bee4186b58e5f879685f500eb4323af398c6
marcbeep <77645923+marcbeep@users.noreply.github.com>
2025-07-07 22:16:10 +0100

    Fixed apos err

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -71,7 +71,7 @@ export default function LoginPage() {
             </Button>
             <div className="text-center">
               <p className="text-sm text-muted-foreground">
-                Don't have an account?{" "}
+                Don&apos;t have an account?{" "}
                 <Link
                   href="/register"
                   className="text-primary font-medium hover:underline"
